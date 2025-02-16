eb9e5d90f32d489ca6cc50e3bc98699763ec4e35
Yerassyl <babayevyerasyl@gmail.com>
2025-02-16 21:52:51 +0500

    lab4

---
+++ Lab4/Part1/ans13/app.routes.ts
@@ -0,0 +1,11 @@
+import {Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {UserComponent} from './user/user.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'App Home Page',
+    component: HomeComponent,
+  },
+];
