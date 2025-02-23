da51b0aed41269e781782034b54b3ca25bf98566
Yerassyl <babayevyerasyl@gmail.com>
2025-02-23 21:47:03 +0500

    Lab5

--- Lab5/OnlineStore/src/app/product-item/product-item.component.ts
+++ Lab5/OnlineStore/src/app/product-item/product-item.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../data';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.css'],
   standalone: true,
+  encapsulation: ViewEncapsulation.None,
 })
 export class ProductItemComponent {
   @Input() product!: Product;
@@ -21,5 +23,19 @@ export class ProductItemComponent {
     } else {
       console.error('Product is undefined in ProductItemComponent');
     }
+  }
+
+  shareOnWhatsApp(product: Product): void {
+    window.open(
+      `https://wa.me/?text=Check out this product: ${product.link}`,
+      '_blank'
+    );
+  }
+
+  shareOnTelegram(product: Product): void {
+    window.open(
+      `https://t.me/share/url?url=${product.link}&text=Check out this product!`,
+      '_blank'
+    );
   }
 }
