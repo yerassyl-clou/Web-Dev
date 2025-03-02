aaad8e21e37ab283fc3a72ca4e45ce3a9d843009
Yerassyl <babayevyerasyl@gmail.com>
2025-03-02 17:36:51 +0500

    upd

--- Lab5/OnlineStore/src/app/app-routing.module.ts
+++ Lab5/OnlineStore/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { ProductDescriptionComponent } from "../product-description/product-description.component";
+import { ProductDescriptionComponent } from "./product-description/product-description.component";
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: "description/:id", component: ProductDescriptionComponent },
   { path: "", redirectTo: "home", pathMatch: "full" },
 ];
